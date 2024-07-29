57222e6e44977d4e8a8f5748567d3c1d405a65ba
aarjan222 <077bei004.aarjan@pcampus.edu.np>
2024-07-29 18:38:54 +0545

    add register&login with username and password, refactor

--- backend.py
+++ backend.py
@@ -1,10 +1,13 @@
+from flask import Flask, render_template, redirect, request, flash, session, url_for
+from datetime import timedelta
+
+import pandas as pd
 import plotly.io as pio
 import plotly.express as px
-from flask import Flask, render_template, redirect, request, flash, session, url_for
-from flask_sqlalchemy import SQLAlchemy
-from datetime import timedelta
+
 import boto3
-import pandas as pd
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
 app.secret_key = "helloworld"
@@ -20,15 +23,22 @@ s3 = boto3.resource(
     aws_secret_access_key='OKJrX6F5xPSU/VZyvGWLaSuU/fb7otfZLuhGrDje'
 )
 
+# Initialize the database
 db = SQLAlchemy(app)
+USERS = {}
+
+with app.app_context():
+    db.create_all()
 
 
 class users(db.Model):
     _id = db.Column("id", db.Integer, primary_key=True)
     name = db.Column("name", db.String(100))
+    password = db.Column("password", db.String(100))
 
-    def __init__(self, name):
+    def __init__(self, name, password):
         self.name = name
+        self.password = password
 
 
 @app.route('/')
@@ -37,39 +47,64 @@ def home():
     return render_template("index.html")
 
 
+@app.route('/register', methods=["POST", "GET"])
+def register():
+    # ---------------To clear all the Database--------------------------
+    # users.__table__.drop(db.engine)
+    # db.create_all()
+    if request.method == "POST":
+        name = request.form['nm']
+        password = request.form['password']
+
+        # check if this user already registered:
+        if users.query.filter_by(name=name).first():
+            flash(f"User already exists")
+            return redirect(url_for('register'))
+
+        # Hash the password
+        hashed_password = generate_password_hash(
+            password, method='pbkdf2:sha256')
+        new_user = users(name, hashed_password)
+        db.session.add(new_user)
+        db.session.commit()
+
+        return redirect(url_for('login'))
+    return render_template('register.html')
+
+
 @app.route('/login', methods=["POST", "GET"])
 def login():
     if request.method == "POST":
-        session.permanent = True
-        user = request.form['nm']
-        session['user'] = user
-        flash(f"Login Successful, {user}", "info")
+        user_name = request.form['nm']
+        user_password = request.form['password']
 
-        usr = users(user)
-        db.session.add(usr)
-        db.session.commit()
+        user = users.query.filter_by(name=user_name).first()
 
-        return redirect(url_for("user"))
+        if user and check_password_hash(user.password, user_password):
+            session.permanent = True
+            session['user'] = user_name
+            flash(f"Login Successful, {user_name}", "info")
+
+            return redirect(url_for('dashboard'))
+        flash(f"Invalid Credentials!")
+        return render_template('login.html')
+
     else:
         if "user" in session:
-            # TODO: to switch login user instead of redirecting to user and login page
             flash(f"Already Logged in by, {session['user']}", "info")
-            return redirect(url_for("user"))
-        flash(f"Create Account first", "info")
+            return redirect(url_for("dashboard"))
 
+        # flash(f"Login first")
         return render_template("login.html")
 
 
-@app.route('/user')
-def user():
+@app.route('/dashboard')
+def dashboard():
     if "user" in session:
-        print("logged in")
-        user = session['user']
-        flash(f'Current User {user}')
-        return render_template("user.html", user=user)
+        user_name = session['user']
+        return render_template("dashboard.html", user=user_name)
     else:
-        flash(f"No user, first login", "info")
-
+        flash(f"First Login")
         return redirect(url_for("login"))
 
 
@@ -78,15 +113,17 @@ def logout():
     if "user" in session:
         user = session['user']
         session.pop("user", None)
+        session.pop("password", None)
         flash(f"You have been logged out, {user}!", "info")
-
+    flash(f"First Login")
     return redirect(url_for("login"))
 
 
 @app.route('/monitor')
 def monitor():
     if 'user' in session:
-        param1 = request.args.get('param1')
+        param1 = session['user']
+        print("current user = ", param1)
 
         # -----------------------Do Only Once when needed-----------------------------------------------
         # uploading datas to s3 buckets
@@ -98,7 +135,9 @@ def monitor():
         #     's3-to-local').download_file(Key='crop_data.csv', Filename='crop_data.csv')
         # ----------------------------------------------------------------------------------------------
 
-        greenhouse_data = pd.read_csv('crop_data.csv')
+        # ---------------------------------change the file location as your directory--------------------
+        greenhouse_data = pd.read_csv(
+            '~/pajor_mroject/flask/gh_display_platforms/crop_data.csv')
 
         # charts -> plotly figures -> html strings
 
@@ -115,13 +154,16 @@ def monitor():
         return render_template('monitor.html', r=param1, data=greenhouse_data.to_dict(orient='records'),
                                temperature_chart=temp_chart, humidity_chart=hum_chart)
     else:
-        flash(f'First create account using login page')
-        return render_template('login.html')
+        flash(f"First Login")
+        return redirect(url_for('login'))
 
 
 @app.route('/control')
 def control():
-    return render_template('control.html')
+    # TODO
+    if 'user' in session:
+        param1 = session['user']
+        return render_template('control.html', r=param1)
 
 
 @app.route('/view')
@@ -133,8 +175,9 @@ def view():
         else:
             print('only admin can view this page')
             flash(f'Cant view users information, login as admin account')
-            return redirect(url_for("user"))
+            return redirect(url_for("dashboard"))
     else:
+        flash(f"First Login")
         return redirect(url_for('login'))
 
 
@@ -147,6 +190,4 @@ def delete(usr):
 
 
 if __name__ == "__main__":
-    with app.app_context():
-        db.create_all()
     app.run(debug=True)
